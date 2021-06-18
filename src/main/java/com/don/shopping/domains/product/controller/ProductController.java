911aed1ff51be9571ef0a92037f243a597bdfe15
wanya <cys567599@gmail.com>
2021-06-18 09:39:54 +0900

    210618 윤서 상품수정진행중2

--- src/main/java/com/don/shopping/domains/product/controller/ProductController.java
+++ src/main/java/com/don/shopping/domains/product/controller/ProductController.java
@@ -1,35 +1,28 @@
 package com.don.shopping.domains.product.controller;
 
 import com.don.shopping.domains.product.domain.ProductEntity;
-import com.don.shopping.domains.product.service.AdminProductListResponseDto;
-import com.don.shopping.domains.product.service.ProductImageResponseDto;
-import com.don.shopping.domains.product.service.ProductImageService;
-import com.don.shopping.domains.product.service.ProductService;
-import com.don.shopping.domains.review.controller.ReviewForm;
-import com.don.shopping.domains.review.domain.ReviewEntity;
-import com.don.shopping.domains.review.service.ReviewService;
-import lombok.AllArgsConstructor;
+import com.don.shopping.domains.product.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@AllArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
     private final ProductImageService productImageService;
-    private final ReviewService reviewService;
 
+    public ProductController(ProductService productService, ProductImageService productImageService) {
+        this.productService = productService;
+        this.productImageService = productImageService;
+    }
 
-    //개별 조회 (리뷰작성,후기작성)
+    /*//개별 조회
     @GetMapping("/products/{id}")
     public String findById(@PathVariable Long id, Model model) {
 
@@ -45,48 +38,20 @@ public class ProductController {
 
         ProductEntity product = productService.searchById(id);
 
-        List<ReviewEntity> reviewsByProductId = reviewService.findReviewsByProductId(id); //+무룡파트 해당 product에 모든 Review 출력
-        model.addAttribute("id",id);//+무룡파트 post방식으로 올때 id
-        model.addAttribute("reviewForm", new ReviewForm()); //+무룡파트 reviewform형식을 product.html에 뿌려줌
         model.addAttribute("product",productService.findOne(id,productimageId));
-        model.addAttribute("reviewListByProduct",reviewsByProductId); //+무룡파트 해당 product에 모든 Review 출력
-        return "customer/products/product.html";
-    }
 
-    //개별 조회 (리뷰작성,후기작성)
-    @PostMapping("/products/{id}")
-    public String addProductViewPost(Model model, @PathVariable Long id,@Valid ReviewForm reviewForm, BindingResult result) { //ReviewForm클래스에 notEmpty가져옴 @Valid
+        return "customer/products/product11.html";
+    }*/
 
-        if(result.hasErrors())
-            return "customer/products/product.html";
+    //개별 조회
+    @GetMapping("/products/{id}")
+    public String findById(@PathVariable Long id, Model model) {
 
-        ReviewEntity reviewEntity = new ReviewEntity();
-        reviewEntity.setContent(reviewForm.getContent());
-        reviewEntity.setRating(reviewForm.getRating());
-        reviewEntity.setProductId(reviewForm.getProductId());
-        Long reviewId = reviewService.addReview(reviewEntity);
-
-        //상품id로 해당 상품 첨부파일 전체 조회
-        List<ProductImageResponseDto> productImageResponseDtoList =
-                productImageService.findAllByProduct(id);
-        //상품 첨부파일 id 담을 List객체 생성
-        List<Long> productimageId = new ArrayList<>();
-        //각 첨부파일 id추가
-        for(ProductImageResponseDto productImageResponseDto : productImageResponseDtoList) {
-            productimageId.add(productImageResponseDto.getFileId());
-        }
-
-        ProductEntity product = productService.searchById(id);
-
-        List<ReviewEntity> reviewsByProductId = reviewService.findReviewsByProductId(id); //+무룡파트 해당 product에 모든 Review 출력
-        model.addAttribute("reviewForm", new ReviewForm()); //+무룡파트 reviewform형식을 product.html에 뿌려줌
-        model.addAttribute("product",productService.findOne(id,productimageId));
-        model.addAttribute("reviewListByProduct",reviewsByProductId); //+무룡파트 해당 product에 모든 Review 출력
+        ProductResponseDto productResponseDto = productService.getProductById(id);
+        model.addAttribute("dto",productResponseDto);
 
         return "customer/products/product.html";
     }
-
-
 
     //전체 조회(목록)
     @GetMapping("/products")
@@ -108,6 +73,18 @@ public class ProductController {
         return "customer/products/productList.html";
     }
 
+    //상품조회(키워드기준)
+    @GetMapping("/products/search")
+    public String findByKeyword(@RequestParam("keyword") String keyword, Model model) {
+        List<ProductEntity> productEntityList = productService.findByKeyword(keyword);
+        List<ProductListResponseDto> responseDtoList = new ArrayList<>();
 
+        for(ProductEntity productEntity : productEntityList) {
+            ProductListResponseDto ProductListResponseDto = new ProductListResponseDto(productEntity);
+            responseDtoList.add(ProductListResponseDto);
+        }
 
+        model.addAttribute("responseDtoList",responseDtoList);
+        return "customer/products/productList.html";
+    }
 }
