330598174cfa68fedaab3093f0d9f3a2c8323648
wanya <cys567599@gmail.com>
2021-06-30 01:06:39 +0900

    210630 html image edit2

--- src/main/java/com/don/shopping/domains/product/controller/ProductController.java
+++ src/main/java/com/don/shopping/domains/product/controller/ProductController.java
@@ -44,9 +44,8 @@ public class ProductController {
         model.addAttribute("reviewForm", new ReviewForm()); //+무룡파트 reviewform형식을 product.html에 뿌려줌
         model.addAttribute("reviewListByProduct", reviewsByProductId); //+무룡파트 해당 product에 모든 Review 출력
         model.addAttribute("id", productId);
-
-        List<QuestionResponseDto> questionList = questionService.findQuestionsByProductId(productId);
-        model.addAttribute("questionList",questionList); //+무룡파트 해당상품에 대한 질문 출력
+        List<QuestionResponseDto> questionEntityList = questionService.findQuestionsByProductId(productId);
+        model.addAttribute("questionList",questionEntityList); //+무룡파트 해당상품에 대한 질문 출력
         model.addAttribute("questionForm", new QuestionForm()); //+무룡파트 해당 상품에 대한 질문추가 form
 
         model.addAttribute("ratingCount",reviewService.ratingCount(productId)); //+무룡파트 해당 상품에 대한 별점의 개수
@@ -83,7 +82,6 @@ public class ProductController {
                 .userId(userId)
                 .content(questionForm.getQuestionContent())
                 .productId(productId)
-                .answer("미답변")
                 .build();
         Long questionId = questionService.addQuestion(questionEntity);
         return "redirect:/products/" + productId;
